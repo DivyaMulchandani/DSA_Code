376e3a723ac83648dcfa7198eddf37693b8498a3
DivyaMulchandani <113537199+DivyaMulchandani@users.noreply.github.com>
2024-11-11 01:38:14 +0530

    Update radix.cpp

--- Searching and sorting/radix.cpp
+++ Searching and sorting/radix.cpp
@@ -74,4 +74,90 @@ int main()
     radixsort(arr, n);
     print(arr, n);
     return 0;
+} /*
+ radix sort
+ */
+/*
+Radix Sort is a linear sorting algorithm that sorts elements by processing them digit by digit. It is an efficient sorting algorithm for integers or strings with fixed-size keys.
+*/
+
+/*
+Find the largest element in the array, which is 802. It has three digits, so we will iterate three times, once for each significant place.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int getMax(int arr[], int n)
+{
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+void countsort(int arr[], int n, int exp)
+{
+    int output[n];
+    int i, count[10] = {0};
+
+    for (i = 0; i < n; i++)
+    {
+        count[(arr[i] / exp) % 10]++;
+    }
+    for (i = 1; i < 10; i++)
+    {
+        count[i] += count[i - 1];
+    }
+    for (i = n - 1; i >= 0; i--)
+    {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+    for (i = 0; i < n; i++)
+    {
+        arr[i] = output[i];
+    }
+}
+
+void radixsort(int arr[], int n)
+{
+    int max = getMax(arr, n);
+    for (int exp = 1; max / exp > 0; exp *= 10)
+    {
+        countsort(arr, n, exp);
+    }
+}
+
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    radixsort(arr, n);
+    cout << "Sorted array: ";
+    print(arr, n);
+    return 0;
 }
